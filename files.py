3601bc5deb425073d43635db1811c6152c05fb6a
xiaoyulong <xiaoyulong1988@gmail.com>
2013-04-05 12:10:20 +0800

    files

--- files.py
+++ files.py
@@ -1,2 +1,13 @@
 __author__ = 'Administrator'
 #文件也可以看做Python中的数据类型
+#open(filename,mode,bufsize)
+# filename 文件名
+#打开模式 r 读方式，w，写方式打开，b，以二进制方式打开文件
+#可选参数，缓冲区大小
+
+#file.read() 整个文件读入字符串中
+#file.write() 向文件中写入字符串
+#.readline()读入文件的一行字符
+#.writeline() 向文件中写入以个列表
+#.readlines()读入文件到一个列表中
+#.close() 关闭打开的文件

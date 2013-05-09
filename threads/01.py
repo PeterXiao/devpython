230ee85133283543221f617afe412c1de3d717e4
xiaoyulong <xiaoyulong1988@gmail.com>
2013-05-09 22:38:30 +0800

    test

--- threads/01.py
+++ threads/01.py
@@ -9,7 +9,11 @@ class CountThread(threading.Thread):
             TOTAL = TOTAL + 1
             MY_LOCK.release()
         print('%s\n' % (TOTAL))
-a = CountThread()
-b = CountThread()
-a.start()
-b.start()
+#a = CountThread()
+#b = CountThread()
+#a.start()
+#b.start()
+from random import random
+
+n=10**6
+print sum(1 if random()**2 + random()**2 < 1 else 0 for i in range(n))*4.0/n

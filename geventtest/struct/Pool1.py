41d79fe320b315e281a5501bcdcb11f1bc8917ad
Administrator <xiaoyulong1988@hotmail.com>
2014-02-27 13:41:09 +0800

    something

---
+++ geventtest/struct/Pool1.py
@@ -0,0 +1,10 @@
+__author__ = 'Administrator'
+import gevent
+from gevent.pool import Pool
+
+pool = Pool(2)
+
+def hello_from(n):
+    print('Size of pool %s' % len(pool))
+
+pool.map(hello_from, xrange(3))

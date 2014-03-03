83bbfd52e91f15ad35e6e6bfe8cb5f99946fd6e6
Administrator <xiaoyulong1988@hotmail.com>
2014-03-03 19:30:01 +0800

    something

---
+++ decorator/de02.py
@@ -0,0 +1,46 @@
+﻿__author__ = 'Administrator'
+"""从心而论，decorator 只是将函数作为参数的函数。
+通常，它的作用是返回封装好的经过修饰的函数。
+下面这个简单的身份识别 decorator
+ 可以让我们了解 decorator 是如何工作的。"""
+def identity_decorator(func):
+    def wrapper():
+        func()
+    return wrapper
+
+
+def a_function():
+    print "I'm a normal function."
+
+# `decorated_function` is the function that `identity_decorator` returns, which
+# is the nested function, `wrapper`
+decorated_function = identity_decorator(a_function)
+
+# This calls the function that `identity_decorator` returned
+decorated_function()
+# >> I'm a normal function
+
+"""
+在这里，identity_decoratordoes 并没有修改其封装的函数。
+它仅仅是返回了这个函数，
+调用了作为参数传递给 identity_decorator 的函数。
+这个 decorator 毫无意义！
+有趣的是在 identity_decorator 中，
+虽然函数没有传递给 wrapper ，
+它依然那可以被调用，这是因为闭包原理。
+"""
+
+
+"""
+闭包
+
+闭包是个花哨的术语，意思是当一个函数被声明，
+在其被声明的词法环境中，它都可以被引用。
+上例中，当 wrapper 被定义时，
+它就访问了本地环境中的函数变量。
+一旦 identity_decorator 返回，
+你就只能通过 decorated_function 访问函数。
+在 decorated_function 的闭包环境之外，
+函数并非以变量形式存在的。
+
+"""
---
+++ decorator/de04.py
@@ -0,0 +1,41 @@
+﻿__author__ = 'Administrator'
+# In the previous example, we used our decorator function by passing the
+# function we wanted to modify to it, and assigning the result to a variable
+def identity_decorator(func):
+    # Everything here happens when the decorator LOADS and is passed
+    # the function as described in step 2 above
+    def wrapper():
+        # Things here happen each time the final wrapped function gets CALLED
+        func()
+    return wrapper
+
+def logging_decorator(func):
+    def wrapper():
+        wrapper.count += 1
+        print "The function I modify has been called {0} times(s).".format(
+              wrapper.count)
+        func()
+    wrapper.count = 0
+    return wrapper
+
+def some_function():
+    print "I'm happiest when decorated."
+
+# Here we will make the assigned variable the same name as the wrapped function
+some_function = logging_decorator(some_function)
+"""
+Decorator语法的简要工作原理:
+当Python的解释器看到这个被装饰的方法时，
+先编译这个方法(some_function), 
+然后先给它赋一个名字 'some_function'.
+这个方法(some_function)再被传入装饰方法(decorator function)logging_decorator中
+装饰方法(decorator function)logging_decorator返回的新方法替代原来的some_function方法, 
+与'some_function'这个名字绑定.
+"""
+
+'''
+希望这里的注释能起到一定的引导作用. 
+只有在装饰器所返回的方法中的指令才会在每次调用的时候被执行. 
+在被返回函数外的指令只会被执行一次-- 在第二步 当装饰器第一次接受一个方法的时候。
+
+'''

8140a40967bad27fc926d04795248446a093f721
PerterShow <xiaoyulong1988@gmail.com>
2017-10-19 18:25:27 +0800

    radixsort

---
+++ alg/sort/selectsort.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：test*.py
+
+
+
+def select_sort(lists):
+    # 选择排序
+    count = len(lists)
+    for i in range(0, count):
+        min = i
+        for j in range(i + 1, count):
+            if lists[min] > lists[j]:
+                min = j
+        lists[min], lists[i] = lists[i], lists[min]
+    return lists

397a1be333e24a54a5baeb91a258e1c47f04e252
PerterShow <xiaoyulong1988@gmail.com>
2017-10-21 20:47:10 +0800

    radixsort

---
+++ base/start/data/linklist.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    # in python next is a reversed word
+    def reverse(self, head):
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
+
+class DListNode:
+    def __init__(self, val):
+        self.val = val
+        self.prev = self.next = null
+
+    def reverse(self, head):
+        curt = None
+        while head:
+            curt = head
+            head = curt.next
+            curt.next = curt.prev
+            curt.prev = head
+        return curt

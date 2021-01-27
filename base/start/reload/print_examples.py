1737a687951a9e6d271d34c4dafed16c3cfefe1a
petershowx <xiaoyulong1988@gmail.com>
2021-01-27 14:10:25 +0800

    温故知新

---
+++ base/start/reload/print_examples.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print(123)
+
+print(123*123)
+
+print("516489")
+
+print('qwert')
+
+"""print words"""
---
+++ base/start/reload/rewords.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('hello\nworld')
+print('hello\tworld')
+print('helloooo\tworld')

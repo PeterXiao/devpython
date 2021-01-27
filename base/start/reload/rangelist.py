d4c308ceafb6c787571bd6421ed51e962256c133
petershowx <xiaoyulong1988@gmail.com>
2021-01-27 15:22:49 +0800

    base\start\reload\rangelist.py

---
+++ base/start/reload/rangelist.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @author xiaoyulong07@outlook.com
+# @version 3
+
+print(range(100))
+print(list(range(100)))
+# for i in range(100):
+#     print(i)
+print(range(2,100))
+print(list(range(2,100)))
+print(list(range(2,100,2)))
